86574498b97665103f4290a1bd5086199156d6d6
Casey Duckering <cduckering@gmail.com>
2019-03-31 00:14:24 -0500

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import logging
 logger = logging.getLogger(__name__)
 
-version = '0.0.1'
+version = '0.1.0'
 
 try:
     with open('README.md', 'r') as f:
